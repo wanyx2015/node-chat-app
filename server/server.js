27f306c7899670a99436d5251f933610150334ef
Wan <yuxi.wan@gmail.com>
2017-10-27 23:05:31 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,10 +14,32 @@ var io = socketIO(server);
 io.on("connection", (socket) => {
     console.log("New user connected.");
 
+    // emit to all client, excluding the emitter
+    socket.emit("newMessage", {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        // createdAt: new Date().getTime()
+    })
+
+    // tell other people new people joined
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "New user joined.",
+        createdAt: new Date().getTime()
+    })
+
     socket.on("createMessage", (message) => {
         console.log("createMessage", message);
 
-        io.emit("newMessage", {
+        // emit to all client, including the emitter
+        // io.emit("newMessage", {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+
+        // emit to all client, excluding the emitter
+        socket.broadcast.emit("newMessage", {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
