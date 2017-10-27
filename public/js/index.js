f3780efba1b454604709b03cbb599422ba7845d9
Wan <yuxi.wan@gmail.com>
2017-10-27 22:35:04 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function () {
     console.log("Connected to server.");
-
-    socket.emit("createMessage", {
-        from: "Steve",
-        text: "You dropped?",
-    })
 })
 
 socket.on("newMessage", function (message) {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ var io = socketIO(server);
 io.on("connection", (socket) => {
     console.log("New user connected.");
 
-    socket.emit("newMessage", {
-        from: "Bill",
-        text: "Good morning!",
-        createdAt: new Date().getTime()
-    })
-
     socket.on("createMessage", (message) => {
         console.log("createMessage", message);
+
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on("disconnect", () => {
